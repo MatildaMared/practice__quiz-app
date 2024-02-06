be8c7bceb009e5dada8acb6dc80eb0cfc2bcbeb5
Matilda Mared <matildamared@live.se>
2024-02-06 08:10:36 +0100

    work on answering logic

--- src/components/Questions.tsx
+++ src/components/Questions.tsx
@@ -6,8 +6,37 @@ interface Props {
 	quiz: Quiz;
 }
 
+interface Answer {
+	question: number;
+	answer: string;
+	isCorrect: boolean;
+}
+
 function Questions({ quiz }: Props) {
 	const [currentQuestion, setCurrentQuestion] = React.useState(0);
+	const [answers, setAnswers] = React.useState<Answer[]>([]);
+
+	const handleAnswer = (answer: string) => {
+		const realAnswer = quiz.questions[currentQuestion].answer;
+		const isCorrect = realAnswer === answer;
+
+		setAnswers([
+			...answers,
+			{
+				question: currentQuestion,
+				answer,
+				isCorrect,
+			},
+		]);
+
+		console.log(answers)
+
+		if (currentQuestion + 1 < quiz.questions.length) {
+			setCurrentQuestion((prev) => prev + 1);
+		} else {
+			console.log(answers);
+		}
+	};
 
 	return (
 		<div>
@@ -16,7 +45,7 @@ function Questions({ quiz }: Props) {
 				{/* TODO: Continue here */}
 				{quiz.questions[currentQuestion].options.map((answer) => (
 					<li key={answer}>
-						<button>{answer}</button>
+						<button onClick={() => handleAnswer(answer)}>{answer}</button>
 					</li>
 				))}
 			</ul>
