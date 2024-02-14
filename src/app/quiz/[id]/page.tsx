d2702c80c2efc1824472d651beb19232e5390b8b
Matilda Mared <matildamared@live.se>
2024-02-14 08:07:28 +0100

    work on quiz details page

--- src/app/quiz/[id]/page.tsx
+++ src/app/quiz/[id]/page.tsx
@@ -2,10 +2,10 @@ import { getQuizById } from "@/utils/quiz";
 import React from "react";
 import Image from "next/image";
 import Questions from "@/components/Questions";
+import styles from "./page.module.css";
 
 export default function Page({ params }: { params: { id: string } }) {
 	const quiz = getQuizById(params.id);
-	console.log(quiz);
 
 	if (!quiz) {
 		return <div>Quiz not found</div>;
@@ -13,13 +13,17 @@ export default function Page({ params }: { params: { id: string } }) {
 
 	return (
 		<div>
-			<h1>{quiz.title}</h1>
-			<Image
-				src={`./../${quiz.icon}`}
-				alt={quiz.title}
-				width={30}
-				height={30}
-			/>
+			<header className={styles.header}>
+				<div className={styles.icon}>
+					<Image
+						src={`./../${quiz.icon}`}
+						alt={quiz.title}
+						width={36}
+						height={36}
+					/>
+				</div>
+				<h1>{quiz.title}</h1>
+			</header>
 			<Questions quiz={quiz} />
 		</div>
 	);
